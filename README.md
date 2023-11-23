392bf6f19c715c1cb02765a2ccb5f45e73ddbf22
Nishant Shah <nishantcshah13@gmail.com>
2023-11-23 13:22:36 +0530

    fix: cleanup

---
+++ README.md
@@ -0,0 +1,7 @@
+## SEED Prisma
+
+npx prisma db seed -- --environment development
+
+## Prisma Stduio 
+
+npx prisma studio --schema prisma/schema1.prisma
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,65 @@
 import { PrismaClient as PrismaClient2 } from '../prisma/generated/client2'
+import { parseArgs } from 'node:util'
+
 const prisma = new PrismaClient2()
+const options = {
+    environment: { type: 'string' },
+} as const
+
+
+const posts = [
+  {
+    title: 'Follow Prisma on Twitter',
+    content: 'https://twitter.com/prisma',
+    published: true,
+  },
+  {
+    title: 'Follow Nexus on Twitter',
+    content: 'https://twitter.com/nexusgql',
+    published: true,
+  },
+]
+
+
 async function main() {
-  const alice = await prisma.user.upsert({
-    where: { email: 'alice@prisma.io' },
-    update: {},
-    create: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      posts: {
-        create: {
-          title: 'Check out Prisma with Next.js',
-          content: 'https://www.prisma.io/nextjs',
-          published: true,
-        },
-      },
-    },
-  })
-  const bob = await prisma.user.upsert({
-    where: { email: 'bob@prisma.io' },
-    update: {},
-    create: {
-      email: 'bob@prisma.io',
-      name: 'Bob',
-      posts: {
-        create: [
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma',
-            published: true,
-          },
-          {
-            title: 'Follow Nexus on Twitter',
-            content: 'https://twitter.com/nexusgql',
-            published: true,
-          },
-        ],
-      },
-    },
-  })
-  console.log({ alice, bob })
+    const {
+        values: { environment },
+    } = parseArgs({ options })
+
+      switch (environment) {
+        case 'development':
+          /** data for your development */
+          const alice = await prisma.user.upsert({
+            where: { email: 'alice@prisma.io' },
+            update: {},
+            create: {
+              email: 'alice@prisma.io',
+              name: 'Alice',
+              posts: {
+                create: posts,
+              },
+            },
+          })
+          const bob = await prisma.user.upsert({
+            where: { email: 'bob@prisma.io' },
+            update: {},
+            create: {
+              email: 'bob@prisma.io',
+              name: 'Bob',
+              posts: {
+                create: posts,
+              },
+            },
+          })
+          console.log({ alice, bob })
+          break
+        case 'test':
+          /** data for your test environment */
+          break
+        default:
+          break
+      }
+  
 }
 main()
   .then(async () => {
